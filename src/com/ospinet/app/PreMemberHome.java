fa58a2c97240bc804bb9efe312f221a0ea0d9a31
rahulpatil4 <ravoolp@gmail.com>
2015-06-23 08:41:55 +0530

    Changes to show user name

--- src/com/ospinet/app/PreMemberHome.java
+++ src/com/ospinet/app/PreMemberHome.java
@@ -47,167 +47,230 @@ import com.devspark.sidenavigation.SideNavigationView.Mode;
 import com.ospinet.app.R;
 
 public class PreMemberHome extends SherlockActivity implements ISideNavigationCallback {
-	private SideNavigationView sideNavigationView;
+    private SideNavigationView sideNavigationView;
+    ProgressDialog dialog;
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-	// TODO Auto-generated method stub
-	super.onCreate(savedInstanceState);
-	//this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-setContentView(R.layout.home_new);
-showActionBar();
-sideNavigationView = (SideNavigationView) findViewById(R.id.side_navigation_view);
-sideNavigationView.setMenuItems(R.menu.side_navigation_menu);
-sideNavigationView.setMenuClickCallback(this);
-sideNavigationView.setMode(Mode.LEFT);
+    // rahul
+    TextView txtname;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.home_new);
+// rahul
+        dialog = new ProgressDialog(PreMemberHome.this);
+        txtname = (TextView) findViewById(R.id.txtName);
+        SharedPreferences myPrefs = PreMemberHome.this
+				.getSharedPreferences("remember", MODE_PRIVATE);
+		String fname = myPrefs.getString("fname", null);
+		String lname = myPrefs.getString("lname", null);
+		txtname.setText(fname +" " + lname);
+        //new GetLoginUser().execute();
+        showActionBar();
+        sideNavigationView = (SideNavigationView) findViewById(R.id.side_navigation_view);
+        sideNavigationView.setMenuItems(R.menu.side_navigation_menu);
+        sideNavigationView.setMenuClickCallback(this);
+        sideNavigationView.setMode(Mode.LEFT);
 
-}
-
-@Override
-public void onSideNavigationItemClick(int itemId) {
-	switch(itemId)
-	{
-case R.id.side_navigation_menu_item1:
-    Intent i = new Intent(PreMemberHome.this, LoginActivity.class);
-	i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	i.putExtra("EXIT", true);
-
-	PreMemberHome.this.startActivity(i);
-
-	break;
-
-case R.id.side_navigation_menu_item2:
-     Intent records = new Intent(PreMemberHome.this, Member_Home.class);
-     PreMemberHome.this.startActivity(records);
-
-     break;
-
-case R.id.side_navigation_menu_item3:
-     Intent help = new Intent(PreMemberHome.this, com.ospinet.app.help.class);
-     PreMemberHome.this.startActivity(help);
-
-     break;
-
-        default:
-    return;
     }
-   // finish();
-}
-@Override
-public void onBackPressed() {
-	// TODO Auto-generated method stub
-	Intent i = new Intent(PreMemberHome.this,LoginActivity.class);
-	this.startActivity(i);
-	finish();
 
-}
-public void showMembers(View v)
-{
-	try
-	{
-		Intent intent = new Intent(PreMemberHome.this, Member_Home.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		startActivity(intent);//finish();
+  /*  public class GetLoginUser extends AsyncTask<String, String, String> {
 
-	}
-	catch(Exception ex)
-	{
-		
-	}
-}
-public void logout(View v)
-{
-	try
-	{
-		 Intent i = new Intent(PreMemberHome.this, LoginActivity.class);
-			i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			i.putExtra("EXIT", true);
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Please Wait..");
+            dialog.show();
+            dialog.setCancelable(false);
+        }
 
-			PreMemberHome.this.startActivity(i);
+        @Override
+        protected String doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            String retstring = "";
+            try {
 
-	}
-	catch(Exception ex)
-	{
-		
-	}
-}
-public void help(View v)
-{
-	try
-	{
-		 Intent i = new Intent(PreMemberHome.this, help.class);
-			
-			PreMemberHome.this.startActivity(i);
+                SharedPreferences myPrefs = PreMemberHome.this
+                        .getSharedPreferences("remember", MODE_PRIVATE);
+                String user_id = myPrefs.getString("userid", null);
+                String response = CustomHttpClient
+                        .executeHttpGet("http://ospinet.com/app_ws/android_app_fun/get_login_user_details?user_id="
+                                + user_id);
+                retstring = response.toString();
 
-	}
-	catch(Exception ex)
-	{
-		
-	}
-}
-private void showActionBar() {
-    LayoutInflater inflator = (LayoutInflater) this
-        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-View v = inflator.inflate(R.layout.menu1, null);
-com.actionbarsherlock.app.ActionBar actionBar = getSupportActionBar();
-actionBar.setDisplayHomeAsUpEnabled(false);
-actionBar.setDisplayShowHomeEnabled (false);
-actionBar.setDisplayShowCustomEnabled(true);
-actionBar.setDisplayShowTitleEnabled(false);
-actionBar.setCustomView(v);
-ImageButton imgAdd = (ImageButton) v.findViewById(R.id.add); //it's important to use your actionbar view that you inflated before
-ImageButton imgMenu = (ImageButton) v.findViewById(R.id.options);	
-imgAdd.setOnClickListener(new OnClickListener() {
+            } catch (Exception io) {
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		Intent intent = new Intent(PreMemberHome.this, MemberActivity.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		intent.putExtra("EXIT", true);
-		PreMemberHome.this.startActivity(intent);
-        
-	}
-});
-imgMenu.setOnClickListener(new OnClickListener() {
-	
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		 sideNavigationView.toggleMenu();
-		 RelativeLayout rel = (RelativeLayout) findViewById(R.id.rel);
-         rel.bringChildToFront(sideNavigationView);
-        
-	}
-});
+            }
+            return retstring;
+        }
+        @Override
+        protected void onPostExecute(String retstring) {
+            if (dialog.isShowing())
+                dialog.dismiss();
+            JSONObject jsonResponse = null;
+            try {
+                jsonResponse = new JSONObject(retstring);
 
-ImageButton imgLogo = (ImageButton) v.findViewById(R.id.logo);
-TextView txtLogoName = (TextView) v.findViewById(R.id.logoName);
+                JSONArray jsonMainNode = jsonResponse.optJSONArray("Login user");
+                
+                for (int i = 0; i < jsonMainNode.length(); i++) {
+                    JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+                    String name = jsonChildNode.optString("fname") + " " + jsonChildNode.optString("lname");
+                    // rahul
+                    txtname.setText(name);
+                }
+            }catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+*/
+    @Override
+    public void onSideNavigationItemClick(int itemId) {
+        switch(itemId)
+        {
+            case R.id.side_navigation_menu_item1:
+                Intent i = new Intent(PreMemberHome.this, LoginActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                i.putExtra("EXIT", true);
 
-imgLogo.setOnClickListener(new OnClickListener() {
-	
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		Intent i = new Intent(PreMemberHome.this,PreMemberHome.class);
-		i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-		startActivity(i);
-	}
-});
-txtLogoName.setOnClickListener(new OnClickListener() {
-	
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		Intent i = new Intent(PreMemberHome.this,PreMemberHome.class);
-		i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-		startActivity(i);
-	}
-});
+                PreMemberHome.this.startActivity(i);
+
+                break;
+
+            case R.id.side_navigation_menu_item2:
+                Intent records = new Intent(PreMemberHome.this, Member_Home.class);
+                PreMemberHome.this.startActivity(records);
+
+                break;
+
+            case R.id.side_navigation_menu_item3:
+                Intent help = new Intent(PreMemberHome.this, com.ospinet.app.help.class);
+                PreMemberHome.this.startActivity(help);
+
+                break;
+
+            default:
+                return;
+        }
+        // finish();
+    }
+    @Override
+    public void onBackPressed() {
+        // TODO Auto-generated method stub
+        Intent i = new Intent(PreMemberHome.this,LoginActivity.class);
+        this.startActivity(i);
+        finish();
+
+    }
+    public void showMembers(View v)
+    {
+        try
+        {
+            Intent intent = new Intent(PreMemberHome.this, Member_Home.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);//finish();
+
+        }
+        catch(Exception ex)
+        {
+
+        }
+    }
+    public void logout(View v)
+    {
+        try
+        {
+            Intent i = new Intent(PreMemberHome.this, LoginActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            i.putExtra("EXIT", true);
+
+            PreMemberHome.this.startActivity(i);
+
+        }
+        catch(Exception ex)
+        {
+
+        }
+    }
+    public void help(View v)
+    {
+        try
+        {
+            Intent i = new Intent(PreMemberHome.this, help.class);
+
+            PreMemberHome.this.startActivity(i);
+
+        }
+        catch(Exception ex)
+        {
+
+        }
+    }
+    private void showActionBar() {
+        LayoutInflater inflator = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflator.inflate(R.layout.menu1, null);
+        com.actionbarsherlock.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowHomeEnabled (false);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setCustomView(v);
+        ImageButton imgAdd = (ImageButton) v.findViewById(R.id.add); //it's important to use your actionbar view that you inflated before
+        ImageButton imgMenu = (ImageButton) v.findViewById(R.id.options);
+        imgAdd.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(PreMemberHome.this, MemberActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                PreMemberHome.this.startActivity(intent);
+
+            }
+        });
+        imgMenu.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                sideNavigationView.toggleMenu();
+                RelativeLayout rel = (RelativeLayout) findViewById(R.id.rel);
+                rel.bringChildToFront(sideNavigationView);
+            }
+        });
+
+        ImageButton imgLogo = (ImageButton) v.findViewById(R.id.logo);
+        TextView txtLogoName = (TextView) v.findViewById(R.id.logoName);
+
+        imgLogo.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(PreMemberHome.this,PreMemberHome.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                startActivity(i);
+            }
+        });
+        txtLogoName.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(PreMemberHome.this,PreMemberHome.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                startActivity(i);
+            }
+        });
 
 
 
-}
+    }
 
 
 }
